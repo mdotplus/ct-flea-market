3a03b3a404f45185c46c29cd1b7c26b777a57f32
mdotplus <sun.eat.summer@gmail.com>
2024-12-08 01:41:52 +0900

    ヘッダー部分の作成

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>flea-market</title>
+</head>
+
+<body>
+<header>
+    <a href="{{ route('home') }}">
+        <img src="{{ asset('images/logo.svg') }}" alt="flea-market">
+    </a>
+    <input type="text" name="keyword" value="{{ $keyword ?? '' }}">
+    <nav>
+        <ul>
+            @if (Auth::check())
+                <li><a href="">ログアウト</a></li>
+            @else
+                <li><a href="">ログイン</a></li>
+            @endif
+            <li><a href="">マイページ</a></li>
+        </ul>
+    </nav>
+    <button type="submit">出品</button>
+</header>
+
+<main>
+    @yield ('content')
+</main>
+</body>
+
+</html>
