b6138a76623f12126ae66ae183503e5da698db81
mdotplus <sun.eat.summer@gmail.com>
2024-12-15 23:50:43 +0900

    商品一覧画面でのおすすめタブロジック実装

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ItemService;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
     public function index()
     {
-        return view('items.index');
+        $itemsWithPurchaseStatus = (new ItemService())->getItemsWithPurchaseStatus();
+        return view('items.index', ['items' => $itemsWithPurchaseStatus]);
     }
 
     public function detail($item_id)
--- src/resources/views/items/index.blade.php
+++ src/resources/views/items/index.blade.php
@@ -8,9 +8,37 @@
         </ul>
 
         <div class="tab-content active" id="recommended">
-            <div class="item">商品画像<br>商品名</div>
-            <div class="item">商品画像<br>商品名</div>
-            <div class="item">商品画像<br>商品名</div>
+            @foreach ($items as $item)
+                @if (auth()->check())
+                    @if (auth()->user()->id === $item['item']->user_id)
+                        @continue
+                    @else
+                        <div class="item">
+                            @if ($item['is_purchased'])
+                                <p>購入済</p>
+                            @endif
+                            @if ($item['item']->image_url)
+                                <img src="{{ $item['item']->image_url }}" alt="商品画像">
+                            @else
+                                <p>画像なし</p>
+                            @endif
+                            <p>{{ $item['item']->name }}</p>
+                        </div>
+                    @endif
+                @else
+                    <div class="item">
+                        @if ($item['is_purchased'])
+                            <p>購入済</p>
+                        @endif
+                        @if ($item['item']->image_url)
+                            <img src="{{ $item['item']->image_url }}" alt="商品画像">
+                        @else
+                            <p>画像なし</p>
+                        @endif
+                        <p>{{ $item['item']->name }}</p>
+                    </div>
+                @endif
+            @endforeach
         </div>
         <div class="tab-content" id="mylist">
             <div class="item">商品画像<br>商品名</div>
